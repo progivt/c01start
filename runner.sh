f1112e6625f30d225ce239fdc88f74c37060af73
Александр Павлов <7911562+av-pavlov@users.noreply.github.com>
2021-02-02 13:57:42 +0900

    исправления

--- runner.sh
+++ runner.sh
@@ -1,3 +1,3 @@
-clang++ -std=c++2a *.cpp -o main
+clang++ -std=c++2a main.cpp -o main
 chmod 744 main
 ./main
---
+++ start-01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	float rad, deg;
+
+	cin >> deg;
+	rad = deg / 180 * 3.14159265358;
+	cout << rad << "\n";
+
+	return 0;
+}
